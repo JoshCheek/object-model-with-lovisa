384889bc83a1bba0917c495f5dce31b90c029dd1
Josh Cheek <josh.cheek@gmail.com>
2015-12-09 06:41:27 -0700

    Some refactoring in prep for being able to swap displays

--- lib/spelunk/cli.rb
+++ lib/spelunk/cli.rb
@@ -15,43 +15,83 @@ require 'spelunk'
 
 class Spelunk
   class CLI
-    attr_reader :stdin, :stdout, :stderr, :argv
-    attr_reader :filename, :spelunk, :height, :width
+    DEFAULT_KEYS         = Hash.new :noop
+    DEFAULT_KEYS["q"]    = :exit
+    DEFAULT_KEYS[3.chr]  = :interrupt # C-c
+    DEFAULT_KEYS[4.chr]  = :next      # C-d
+    DEFAULT_KEYS["s"]    = :self
+    DEFAULT_KEYS["l"]    = :locals
+    DEFAULT_KEYS["i"]    = :instance_variables
+    DEFAULT_KEYS["b"]    = :binding
+    DEFAULT_KEYS["c"]    = :callstack
+    DEFAULT_KEYS["\e[A"] = :up
+    DEFAULT_KEYS["\e[B"] = :down
+    DEFAULT_KEYS["\e[C"] = :right
+    DEFAULT_KEYS["\e[D"] = :left
+    DEFAULT_KEYS["\r"]   = :next
+    DEFAULT_KEYS["\n"]   = :next
 
-    def initialize(stdin, stdout, stderr, argv)
-      @stdin    = stdin
-      @stdout   = stdout
-      @stderr   = stderr
-      @argv     = argv
-      @filename = File.expand_path(argv[0])
-      @spelunk  = Spelunk.new filename
-      @height, @width = stdin.winsize
+    DISPLAYS                      = Hash.new { |h, k| raise "No key #{k.inspect} in #{h.inspect}" }
+    DISPLAYS[:callstack]          = :callstack
+    DISPLAYS[:self]               = :self
+    DISPLAYS[:locals]             = :locals
+    DISPLAYS[:instance_variables] = :instance_variables
+    DISPLAYS[:binding]            = :binding
+
+    attr_accessor :stdin, :stdout, :stderr, :argv
+    attr_accessor :filename, :spelunk, :height, :width, :keys
+    attr_accessor :display, :callstack_index
+
+    def initialize(stdin, stdout, stderr, argv, initial_display=:callstack, keys=DEFAULT_KEYS)
+      self.stdin              = stdin
+      self.stdout             = stdout
+      self.stderr             = stderr
+      self.argv               = argv
+      self.keys               = keys
+      self.filename           = File.expand_path(argv[0])
+      self.spelunk            = Spelunk.new filename
+      self.display            = initial_display
+      self.callstack_index    = 0
+      self.height, self.width = stdin.winsize
     end
 
     def event(event)
-      print "\r"
       return unless spelunk.process? event
-      event_name = event[:event]
-      stdout.print "\e[H\e[2J#{highlighted_body event}\n\n\r\e[45m#{event_name}\r\n\n"
-      stdout.print "\e[41;37m Press a key \e[0m"
-      char = stdin.getc
-      direction = :forward
-      if char == "\e"
-        stdin.getc # [
-        direction = {
-          'A' => :backward,
-          'B' => :forward,
-          'C' => :forward,
-          'D' => :backward,
-        }[stdin.getc] # A / B / C / D
-      elsif char == "k"
-        direction = :backward
-      elsif char == "q" || char.ord == 3 # C-c
-        :done
+      display_screen(event)
+      loop do
+        key = keys[stdin.readpartial(100)]
+        case key
+        when :noop, :next
+          break nil
+        when :quit
+          break key
+        when :interrupt
+          Process.kill 'INT', Process.pid
+        when :self, :locals, :instance_variables, :binding, :callstack
+          self.display = key
+        when :up
+          self.callstack_index = [stack.length-1, callstack_index+1].min
+        when :down
+          self.callstack_index = [0, callstack_index-1].max
+        when :right
+          # currently nothing to do
+        when :left
+          # currently nothing to do
+        end
       end
     end
 
     private
+
+    def display_screen(event)
+      topleft    = "\e[H"
+      clear      = "\e[2J"
+      newline    = "\r\n"
+      event_name = "\e[45m#{event[:event]}\e[49m"
+      prompt     = "\e[41;37m Press a key \e[49;37m"
+      output     = "" << topleft << clear << highlighted_body(event) << newline << newline << event_name << newline << prompt
+      stdout.print(output)
+    end
 
     def highlighted_body(event)
       linenum_width = 3
