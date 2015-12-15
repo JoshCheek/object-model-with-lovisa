bad4d154d963143212b0778a8a36e24ebb3f679b
Horace Williams <horace.d.williams@gmail.com>
2015-12-15 10:24:34 -0700

    add a simpler cooking example with pizza

---
+++ examples/cook_pizza.rb
@@ -0,0 +1,22 @@
+def make_pizza
+  toss_dough
+  add_toppings
+  bake
+end
+
+def toss_dough
+  toss_count = rand(8)
+  toss_count.times do
+    puts "Toss the dough"
+  end
+end
+
+def add_toppings
+  puts "add those tasty anchovies"
+end
+
+def bake
+  puts "cook it up in the oven"
+end
+
+make_pizza
